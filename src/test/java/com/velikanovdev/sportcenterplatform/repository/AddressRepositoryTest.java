c2b268c6a3c124d4757a60f4613075b5f8a17919
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-12 22:49:21 +0100

    Fix: KISS

--- src/test/java/com/velikanovdev/sportcenterplatform/repository/AddressRepositoryTest.java
+++ src/test/java/com/velikanovdev/sportcenterplatform/repository/AddressRepositoryTest.java
@@ -2,17 +2,14 @@ package com.velikanovdev.sportcenterplatform.repository;
 
 import com.velikanovdev.sportcenterplatform.entity.Address;
 import com.velikanovdev.sportcenterplatform.job.SaveAddressJob;
-import com.velikanovdev.sportcenterplatform.repository.configuration.AddressRepositoryTestConfiguration;
-import jakarta.annotation.Resource;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = AddressRepositoryTestConfiguration.class)
+@SpringBootTest
 public class AddressRepositoryTest {
-    @Resource
+
+    @Autowired
     private SaveAddressJob saveAddressJob;
 
     @Test
