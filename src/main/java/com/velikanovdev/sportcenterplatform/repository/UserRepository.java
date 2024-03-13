6738229a21eca498aebf685e05c9eb15fae6ec36
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-13 15:37:49 +0100

    feat: add user repository and user register endpoint

---
+++ src/main/java/com/velikanovdev/sportcenterplatform/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.velikanovdev.sportcenterplatform.repository;
+
+import com.velikanovdev.sportcenterplatform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
