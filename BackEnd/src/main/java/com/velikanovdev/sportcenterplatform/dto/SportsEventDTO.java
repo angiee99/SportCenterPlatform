eeb7f0892f2753e58c607fedaaa0632e1e9b33ae
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-19 21:11:48 +0100

    feat: add SportsEvent dto

---
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/dto/SportsEventDTO.java
@@ -0,0 +1,20 @@
+package com.velikanovdev.sportcenterplatform.dto;
+
+import com.velikanovdev.sportcenterplatform.entity.enums.SportType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SportsEventDTO {
+    private Long id;
+    private String description;
+    private boolean isAvailable;
+    private SportType sportType;
+    private String ageRestriction;
+    private String eventTypeDescription;
+    private String venueInfo;
+    private String trainerName;
+}
