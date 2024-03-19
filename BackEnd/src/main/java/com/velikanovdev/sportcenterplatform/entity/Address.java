fb086468a06b27ff825d3b6f7629365dfb7c7ba1
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-19 21:12:41 +0100

    refactor: change toString and lombok annotations

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/Address.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/Address.java
@@ -1,15 +1,16 @@
 package com.velikanovdev.sportcenterplatform.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
-@Getter
-@Setter
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
+@Getter
 @Table(name = "addresses")
 public class Address {
 
@@ -42,21 +43,5 @@ public class Address {
                 ", street='" + street + '\'' +
                 ", streetNumber='" + streetNumber + '\'' +
                 ", city='" + city + '\'';
-    }
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Address address = (Address) o;
-        return getId() != null && Objects.equals(getId(), address.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
