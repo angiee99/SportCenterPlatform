80d9d446535d11b7596d3f1c63666e94c904e44b
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-19 21:07:50 +0100

    feat: add new endpoints (getAllEvents, getActiveEvents)

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/controller/EventController.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/controller/EventController.java
@@ -1,10 +1,13 @@
 package com.velikanovdev.sportcenterplatform.controller;
 
+import com.velikanovdev.sportcenterplatform.dto.SportsEventDTO;
 import com.velikanovdev.sportcenterplatform.entity.SportsEvent;
 import com.velikanovdev.sportcenterplatform.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EventController {
@@ -20,18 +23,37 @@ public class EventController {
         if(event == null) {
             return ResponseEntity.badRequest().build();
         }
-
         SportsEvent createdEvent = eventService.createEvent(event);
         return ResponseEntity.ok("Created event: " + createdEvent);
     }
 
     @GetMapping("/event/{id}")
-    public ResponseEntity<SportsEvent> getEventDetails(@PathVariable Long id) {
-        SportsEvent sportEvent = eventService.showEventDetails(id);
+    public ResponseEntity<SportsEvent> getEvent(@PathVariable Long id) {
+        SportsEvent sportEvent = eventService.getEvent(id);
         if(sportEvent == null) {
             return ResponseEntity.notFound().build();
         }
-
         return ResponseEntity.ok(sportEvent);
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<SportsEventDTO>> getAllEvents() {
+        List<SportsEventDTO> sportsEvents = eventService.getAllEvents();
+
+        if(sportsEvents == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(sportsEvents);
+    }
+
+    @GetMapping("/activeEvents")
+    public ResponseEntity<List<SportsEventDTO>> getActiveEvents() {
+        List<SportsEventDTO> activeEvents = eventService.getActiveEvents();
+
+        if(activeEvents == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(activeEvents);
     }
 }
--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/service/EventService.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/service/EventService.java
@@ -1,10 +1,13 @@
 package com.velikanovdev.sportcenterplatform.service;
 
+import com.velikanovdev.sportcenterplatform.dto.SportsEventDTO;
 import com.velikanovdev.sportcenterplatform.entity.SportsEvent;
 import com.velikanovdev.sportcenterplatform.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,8 +23,50 @@ public class EventService {
         return eventRepository.save(event);
     }
 
-    public SportsEvent showEventDetails(Long id) {
+    public SportsEvent getEvent(Long id) {
         Optional<SportsEvent> sportsEvent = eventRepository.findById(id);
         return sportsEvent.orElse(null);
+    }
+
+    public List<SportsEventDTO> getAllEvents() {
+        List<SportsEvent> sportsEvents = eventRepository.findAll();
+        List<SportsEventDTO> eventDTOS = convertSportsEventToDTO(sportsEvents);
+
+        if(eventDTOS.isEmpty()) {
+            return null;
+        }
+
+        return eventDTOS;
+    }
+
+    public List<SportsEventDTO> getActiveEvents() {
+        List<SportsEvent> sportsEvents = eventRepository.findByIsAvailableTrue();
+        List<SportsEventDTO> eventDTOS = convertSportsEventToDTO(sportsEvents);
+
+
+        if (eventDTOS.isEmpty()) {
+            return null;
+        }
+
+        return eventDTOS;
+    }
+
+    private List<SportsEventDTO> convertSportsEventToDTO(List<SportsEvent> sportsEvents) {
+        List<SportsEventDTO> eventDTOS = new ArrayList<>();
+
+        for(SportsEvent event: sportsEvents) {
+            SportsEventDTO eventDTO = new SportsEventDTO();
+            eventDTO.setId(event.getId());
+            eventDTO.setDescription(event.getDescription());
+            eventDTO.setAvailable(event.isAvailable());
+            eventDTO.setSportType(event.getEventType().getSport());
+            eventDTO.setAgeRestriction(event.getEventType().getAgeRestriction());
+            eventDTO.setEventTypeDescription(event.getEventType().getDescription());
+            eventDTO.setVenueInfo(event.getVenue().toString());
+            eventDTO.setTrainerName(event.getTrainer().getName());
+            eventDTOS.add(eventDTO);
+        }
+
+        return eventDTOS;
     }
 }
