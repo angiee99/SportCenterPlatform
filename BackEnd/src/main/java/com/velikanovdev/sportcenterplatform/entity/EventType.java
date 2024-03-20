b6880144c50e796ab4a77d614d2441eaf0ceadab
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-20 13:42:03 +0100

    fix: generate hashcode and equals manually because @Data for JPA entities is an antipattern

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/EventType.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/EventType.java
@@ -1,18 +1,17 @@
 package com.velikanovdev.sportcenterplatform.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.velikanovdev.sportcenterplatform.entity.enums.SportType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.List;
+import java.util.Objects;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "event_types")
 public class EventType {
@@ -31,11 +30,28 @@ public class EventType {
     private String description;
 
     @OneToMany(mappedBy = "eventType")
+    @ToString.Exclude
     private List<SportsEvent> sportsEvents;
 
     public EventType(SportType sport, String ageRestriction, String description) {
         this.sport = sport;
         this.ageRestriction = ageRestriction;
         this.description = description;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        EventType eventType = (EventType) o;
+        return getId() != null && Objects.equals(getId(), eventType.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
