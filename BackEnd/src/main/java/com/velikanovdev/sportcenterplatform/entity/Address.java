0919a0208b7b106e8637cc221399e8517dc47151
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-20 13:42:03 +0100

    fix: generate hashcode and equals manually because @Data for JPA entities is an antipattern

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/Address.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/Address.java
@@ -1,16 +1,15 @@
 package com.velikanovdev.sportcenterplatform.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Entity
+import java.util.Objects;
+
 @Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
 @Table(name = "addresses")
 public class Address {
 
@@ -43,5 +42,21 @@ public class Address {
                 ", street='" + street + '\'' +
                 ", streetNumber='" + streetNumber + '\'' +
                 ", city='" + city + '\'';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Address address = (Address) o;
+        return getId() != null && Objects.equals(getId(), address.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/Schedule.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/Schedule.java
@@ -1,16 +1,17 @@
 package com.velikanovdev.sportcenterplatform.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "schedules")
 public class Schedule {
@@ -25,17 +26,34 @@ public class Schedule {
     @Column(name = "end_time")
     private LocalDateTime endTime;
 
-    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL)
-    private List<EventSignup> registrations;
+    @OneToMany(mappedBy = "schedule")
+    @ToString.Exclude
+    private List<EventSignup> eventSignups;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sports_event_id", foreignKey = @ForeignKey(name = "FK_schedules_sports_events"))
+    @ToString.Exclude
     private SportsEvent sportsEvent;
 
-    public Schedule( LocalDateTime startTime, LocalDateTime endTime, SportsEvent sportsEvent) {
+    public Schedule(LocalDateTime startTime, LocalDateTime endTime, SportsEvent sportsEvent) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.sportsEvent = sportsEvent;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Schedule schedule = (Schedule) o;
+        return getId() != null && Objects.equals(getId(), schedule.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
