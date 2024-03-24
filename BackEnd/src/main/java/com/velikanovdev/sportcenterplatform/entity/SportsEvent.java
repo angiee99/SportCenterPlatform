281505d12208365f75c15a49b8046afbe83b7bd7
Anhelina Kulkova <angeltutova@gmail.com>
2024-03-24 14:56:16 +0100

    fix: add setter

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/SportsEvent.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/entity/SportsEvent.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
 @Table(name = "sports_events")
 public class SportsEvent {
