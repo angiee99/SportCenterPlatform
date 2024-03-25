157ce068e0dfb0cafbad23867fc2e0b2cb1b1416
Michal Syřiště <michalsyriste@seznam.cz>
2024-03-25 14:36:53 +0100

    Add Details.razor Update Home page

---
+++ FrontEnd/GlobalData.cs
@@ -0,0 +1,8 @@
+using SportCenter.Models;
+
+namespace SportCenter;
+
+public static class GlobalData
+{
+    public static List<TableData> Records { get; set; } = new List<TableData>();
+}
--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -5,7 +5,7 @@ using SportCenter;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddHttpClient();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
