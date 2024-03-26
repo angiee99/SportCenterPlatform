e05b323b75933ae8a1ae41c22791293d6f417c09
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-27 00:47:02 +0100

    feat: delete exception handler, it is global now

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/controller/AuthController.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/controller/AuthController.java
@@ -4,14 +4,8 @@ import com.velikanovdev.sportcenterplatform.dto.UserDTO;
 import com.velikanovdev.sportcenterplatform.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class AuthController {
@@ -29,17 +23,5 @@ public class AuthController {
             return ResponseEntity.badRequest().body("User with email " + userDTO.email() + " already exists");
         }
         return ResponseEntity.ok("User with id " + id + " successfully registered");
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return ResponseEntity.badRequest().body(errors);
     }
 }
