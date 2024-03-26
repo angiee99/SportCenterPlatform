2773f6f4830a716af7231c684da876ac5986afc9
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-26 19:34:04 +0100

    fix: now /register endpoint checks if UserDTO is valid

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/controller/AuthController.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/controller/AuthController.java
@@ -2,11 +2,16 @@ package com.velikanovdev.sportcenterplatform.controller;
 
 import com.velikanovdev.sportcenterplatform.dto.UserDTO;
 import com.velikanovdev.sportcenterplatform.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AuthController {
@@ -18,11 +23,23 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<?> register(@Valid @RequestBody UserDTO userDTO) {
         Long id = userService.registerUser(userDTO);
         if(id == null) {
             return ResponseEntity.badRequest().body("User with email " + userDTO.email() + " already exists");
         }
         return ResponseEntity.ok("User with id " + id + " successfully registered");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
