0a597a4568fa7346eb9dd8831bc2c20061ad5b49
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-28 00:02:28 +0100

    feat: add @Builder annotation

--- BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/dto/UserDTO.java
+++ BackEnd/src/main/java/com/velikanovdev/sportcenterplatform/dto/UserDTO.java
@@ -3,7 +3,9 @@ package com.velikanovdev.sportcenterplatform.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record UserDTO(
         @NotBlank(message = "Email is required") @Email(message = "Email is not valid") String email,
         @NotBlank(message = "Username is required") String username,
